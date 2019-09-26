359694cf6bd9ac80dc604042733bc0257aac7c82
Matt Huston <matthuston1@aol.com>
2019-09-26 14:37:28 -0600

    fighting with apis

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
-fetch("https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3", {
+var origApi = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3"
+var origVoiceApi = "https://audd.p.rapidapi.com/findLyrics/?q=adele%20hello"
+var lyricsApi = "https://api.audd.io/findLyrics/?q=adele%20hello"
+var lyricsWordsApi = "https://api.audd.io/findLyrics/?q=I%27m%20waking%20up%20to%20ash%20and%20dust"
+
+
+fetch(origApi, {
   "method": "GET",
   "headers": {
     "x-rapidapi-host": "audd.p.rapidapi.com",
@@ -15,16 +21,17 @@ fetch("https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cde
 
   // Lyrics Recognition and search
 
-//   fetch("https://audd.p.rapidapi.com/findLyrics/?q=adele%20hello", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "audd.p.rapidapi.com",
-// 		"x-rapidapi-key": "9486a8a6f8mshac9ab31bbfa1a58p1e1886jsn73bd4264373d"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
+  fetch("https://audd.p.rapidapi.com/findLyrics/?q=adele%20hello", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "audd.p.rapidapi.com",
+		"x-rapidapi-key": "9486a8a6f8mshac9ab31bbfa1a58p1e1886jsn73bd4264373d"
+	}
+})
+.then(response => response.json())
+.then(response => {
+  console.log(response)
+})
 // .catch(err => {
 // 	console.log(err);
 // });
