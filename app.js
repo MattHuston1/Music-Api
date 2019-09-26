b5d1c340e7dae6c8d6d944fedd593cd9ebb45f77
Matt Huston <matthuston1@aol.com>
2019-09-26 15:54:53 -0600

    waiting on token, believe I have basic setup for finding lyrics from searching artist name and song name

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-var origApi = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3"
-var origVoiceApi = "https://audd.p.rapidapi.com/findLyrics/?q=adele%20hello"
-var lyricsApi = "https://api.audd.io/findLyrics/?q=adele%20hello"
-var lyricsWordsApi = "https://api.audd.io/findLyrics/?q=I%27m%20waking%20up%20to%20ash%20and%20dust"
+let artistNameSubmitted = document.querySelector("#artistName")
+let songNameSubmitted = document.querySelector("#songName")
+var songForm = document.querySelector("#songForm")
+
+let artistName = ""
+let songName = ""
+
+let origApi = "https://audd.p.rapidapi.com/?return=timecode%2Capple_music%2Cspotify%2Cdeezer%2Clyrics&itunes_country=us&url=https%3A%2F%2Faudd.tech%2Fexample1.mp3"
+let origVoiceApi = "https://audd.p.rapidapi.com/findLyrics/?q=adele%20hello"
+let lyricsApi = `https://galvanize-cors.herokuapp.com/https://api.audd.io/findLyrics/?q=${artistName}%20${songName}`
+let lyricsWordsApi = "https://api.audd.io/findLyrics/?q=I%27m%20waking%20up%20to%20ash%20and%20dust"
 
 
-fetch(origApi, {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "audd.p.rapidapi.com",
-    "x-rapidapi-key": "9486a8a6f8mshac9ab31bbfa1a58p1e1886jsn73bd4264373d"
-  }
-})
-  .then(response => response.json())
-  .then(myJSON => {
-    console.log(myJSON)
+songForm.addEventListener("submit", songFormSubmit)
+
+function songFormSubmit(event) {
+  event.preventDefault()
+
+  artistName = artistNameSubmitted.value
+  songName = songNameSubmitted.value
+
+  fetch(lyricsApi, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "audd.p.rapidapi.com",
+      "x-rapidapi-key": "9486a8a6f8mshac9ab31bbfa1a58p1e1886jsn73bd4264373d"
+    }
   })
-  // .catch(err => {
-  //   console.log(err);
-  // });
+    .then(response => response.json())
+    .then(myJSON => {
+      console.log(myJSON)
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
   // Lyrics Recognition and search
-
-  fetch("https://audd.p.rapidapi.com/findLyrics/?q=adele%20hello", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "audd.p.rapidapi.com",
-		"x-rapidapi-key": "9486a8a6f8mshac9ab31bbfa1a58p1e1886jsn73bd4264373d"
-	}
-})
-.then(response => response.json())
-.then(response => {
-  console.log(response)
-})
-// .catch(err => {
-// 	console.log(err);
-// });
